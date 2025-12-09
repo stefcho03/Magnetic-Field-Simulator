a5e5e6d171e6a4ed1e42b9324ee17f42a4ffa6b0
stefcho03 <uwxfi@student.kit.edu>
2025-12-09 10:42:27 +0100

    Rotation logic implemented

---
+++ Manet Field Simulator/Magnets/Include/helpfunctions.h
@@ -0,0 +1,28 @@
+#ifndef MAGNETS_INCLUDE_HELPFUNCTIONS_
+#define MAGNETS_INCLUDE_HELPFUNCTIONS_
+
+#include <array>
+#include <concepts>
+
+template <typename T>
+concept Quaternion = std::same_as<T, std::array<float, 4>>;
+
+class HelpFunctions
+{
+public:
+    static std::array<float, 4> quaternion (float degrees, std::array<float, 3> axis);
+    static std::array<float, 4> inverseQuaternion (std::array<float, 4> quaternion);
+
+    template <Quaternion... Q>
+    static std::array<float, 4> quaternionMultiplication (Q&... q)
+    {
+        std::array<float, 4> result{ 1, 0, 0, 0 };
+        ((result = quatMul (result, q)), ...);
+        return result;
+    }
+
+private:
+    static std::array<float, 4> quatMul (std::array<float, 4> q1, std::array<float, 4> q2);
+};
+
+#endif // MAGNETS_INCLUDE_HELPFUNCTIONS_
